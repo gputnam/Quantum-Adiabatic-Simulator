bbf58c8be75b45f06ffe0adaccebbce5904926d7
gputnam <graylcputnam@college.harvard.edu>
2015-01-28 17:36:28 -0500

    Deleted unnecessary files.

--- plot_data.m
+++
@@ -1,9 +0,0 @@
-function [plotx,ploty] = plot_data(master_data)
-[r,c,h] = size(master_data);
-plotx = zeros([1,c]);
-ploty = zeros([1,c]);
-for i = 1:c
-    plotx(i) = master_data(1,i,1);
-    ploty(i) = master_data(1,i,2);
-end
-end
